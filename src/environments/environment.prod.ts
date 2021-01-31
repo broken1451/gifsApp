ce6e161208fa08689eb785a0e11a9c9aa96da931
Adrian <adrianbravo145@gmail.com>
2021-01-31 19:48:50 -0300

    App with input y outputs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiKey: 'KEn0sIQmjVnqxSRIxwa071eTC4hxQlHn',
-  url: 'http://api.giphy.com/v1/gifs'
+  url: 'https://api.giphy.com/v1/gifs'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiKey: 'KEn0sIQmjVnqxSRIxwa071eTC4hxQlHn',
-  url: 'http://api.giphy.com/v1/gifs'
+  url: 'https://api.giphy.com/v1/gifs'
 };
 
 /*
